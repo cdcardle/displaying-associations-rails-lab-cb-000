f75da6563a7c4558e1ce887a475f5d5c2e3e8477
Chris Cardle <cdcardle@gmail.com>
2018-06-11 12:37:34 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,12 +2,6 @@ class Artist < ActiveRecord::Base
   has_many :songs
 
   def song_count
-    c = self.songs.count
-    s = ""
-    if c != 1
-      s = "#{c} songs"
-    else
-      s = "1 song"
-    end
+    self.songs.count
   end
 end
